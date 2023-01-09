089bbf56e30a1507e2f00c0439f09b9763e460cb
mmoale021 <mmoale021@student.wethinkcode.co.za>
2023-01-09 14:11:22 +0200

    refactoring game functions

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,7 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize game, settings and create a screen object.
@@ -17,15 +17,7 @@ def run_game():
     while True:
 
         # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        
-        # Redraw the screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
